acfb6dad62cf4202779e16a40d52927c9877e8ef
Blake Feldman <26087892+bfeldman89@users.noreply.github.com>
2023-01-25 04:17:27 -0600

    Update msleg_openstates_snips.py

--- msleg_openstates_snips.py
+++ msleg_openstates_snips.py
@@ -9,38 +9,69 @@ OPENSTATES_API_KEY = 'd1767663-d765-474d-b3ad-98a59eab9b3c'
 pyopenstates.set_api_key(OPENSTATES_API_KEY)
 
 
-
 def get_bills():
     airtable = Airtable('appxbJoRFBRbGgj4s', 'bills', os.environ['AIRTABLE_API_KEY'])
     yesterday = date.today() - timedelta(1)
     yesterday_iso = yesterday.isoformat()
-    bills = pyopenstates.search_bills(state='ms', updated_since=yesterday_iso)
+    # bills = pyopenstates.search_bills(state='ms', updated_since=yesterday_iso)
+    bills = pyopenstates.search_bills(state='ms', session='2023')
     for bill in bills:
-        record = airtable.match('bill_id', bill['bill_id'])
+        # record = airtable.match('bill_id', bill['bill_id'])
+        record = airtable.match('bill_id', bill['identifier'], view='2023')
         this_dict = {}
-        this_dict['bill_id'] = bill['bill_id']
+        # this_dict['bill_id'] = bill['bill_id']
+        this_dict['bill_id'] = bill['identifier']
         this_dict['created_at'] = str(bill['created_at'].isoformat())
         this_dict['updated_at'] = str(bill['updated_at'].isoformat())
         this_dict['title'] = bill['title']
         this_dict['session'] = bill['session']
-        this_dict['type'] = bill['type']
-        this_dict['chamber'] = bill['chamber']
-        this_dict['documents'] = str(bill['documents'])
-        this_dict['scraped_subjects'] = bill['scraped_subjects']
-        this_dict['actions'] = str(bill['actions'])
-        this_dict['actions_count'] = len(bill['actions'])
-        last_action = bill['actions'][len(bill['actions'])-1]
-        this_dict['last_action'] = last_action['action']
-        this_dict['last_action_date'] = last_action['date']
-        this_dict['last_action_type'] = last_action['type']
-        this_dict['last_action_actor'] = last_action['actor']
+        # this_dict['type'] = bill['type']
+        # this_dict['chamber'] = bill['chamber']
+        # this_dict['documents'] = str(bill['documents'])
+        # this_dict['scraped_subjects'] = bill['scraped_subjects']
+        # this_dict['actions'] = str(bill['actions'])
+        # this_dict['actions_count'] = len(bill['actions'])
+        # last_action = bill['actions'][len(bill['actions'])-1]
+        # this_dict['last_action'] = last_action['action']
+        # this_dict['last_action_date'] = last_action['date']
+        # this_dict['last_action_type'] = last_action['type']
+        # this_dict['last_action_actor'] = last_action['actor']
+        this_dict['type'] = bill['classification']
+        this_dict['chamber'] = bill['from_organization']['classification']
+        this_dict['scraped_subjects'] = bill['subject']
+        this_dict['last_action'] = bill['latest_action_description']
+        this_dict['last_action_date'] = bill['latest_action_date']
+        this_dict['first_action_date'] = bill['first_action_date']
+        this_dict['summary'] = bill['extras']['summary']
         if record:
             airtable.update(record['id'], this_dict)
         if not record:
             airtable.insert(this_dict)
+        time.sleep(.5)
 
     print(str(len(bills)) + ' bills updated since yesterday')
 
+
+'''
+example bill data from get_bills:
+
+{'id': 'ocd-bill/3301cb8f-d404-4ea8-83dc-90c9547f76c9', 
+'session': '2023', 
+'jurisdiction': {'id': 'ocd-jurisdiction/country:us/state:ms/government', 'name': 'Mississippi', 'classification': 'state'}, 
+'from_organization': {'id': 'ocd-organization/47c2e037-ffe3-46c3-9399-8feb1f7d4988', 'name': 'House', 'classification': 'lower'}, 
+'identifier': 'HR 27', 
+'title': 'Faye Graham Dillard; commend upon the occasion of her 90th birthday.', 
+'classification': ['resolution'], 
+'subject': ['Rules'], 
+'extras': {'summary': 'A RESOLUTION COMMENDING AND CONGRATULATING MS. FAYE GRAHAM DILLARD UPON THE OCCASION OF HER 90TH BIRTHDAY.'}, 
+'created_at': datetime.datetime(2023, 1, 25, 0, 44, 15, 972151, tzinfo=tzutc()), 
+'updated_at': datetime.datetime(2023, 1, 25, 0, 44, 15, 989042, tzinfo=tzutc()), 
+'openstates_url': 'https://openstates.org/ms/bills/2023/HR27/', 
+'first_action_date': '2023-01-16T06:00:00+00:00', 
+'latest_action_date': '2023-01-16T06:00:00+00:00', 
+'latest_action_description': 'Referred To Rules', 
+'latest_passage_date': ''}
+'''
 
 def get_ppl():
     airtable = Airtable('appxbJoRFBRbGgj4s', 'legislators', os.environ['AIRTABLE_API_KEY'])
